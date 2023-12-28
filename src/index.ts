0878a65a0fcbba17d2e55c113d40e33cf2a2e46a
Yoni Gozman <gozmanyoni@gmail.com>
2023-12-28 11:59:12 +1100

    Export messages from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { SocketMost } from './server/SocketMost'
 import { SocketMostClient } from './client/SocketMost-Client'
 import { OS8104A } from './driver/OS8104A'
-
-export { SocketMost, SocketMostClient, OS8104A }
+import * as messages from './modules/Messages'
+export { SocketMost, SocketMostClient, OS8104A, messages }
