598242a9ccea4581b708f6f56fc1aa00c57eb8a3
rhysmorgan134 <36861722+rhysmorgan134@users.noreply.github.com>
2024-02-27 20:30:15 +0000

    remove winston testing

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,5 @@ import { SocketMost } from './server/SocketMost'
 import { SocketMostClient } from './client/SocketMost-Client'
 import { OS8104A } from './driver/OS8104A'
 import * as messages from './modules/Messages'
-import winston from 'winston'
-const logger = winston.loggers.get('applicationLogger')
 import './log'
-export { SocketMost, SocketMostClient, OS8104A, messages, logger }
+export { SocketMost, SocketMostClient, OS8104A, messages }
